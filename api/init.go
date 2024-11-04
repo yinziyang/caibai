0472960869235e4256898134dbe265a5e3a732e7
yinziyang <yinziyangphone@gmail.com>
2024-11-04 12:46:42 +0800

    Update init.go

--- api/init.go
+++ api/init.go
@@ -9,8 +9,6 @@ import (
 var redisClient *redis.Client
 
 func init() {
-	os.Setenv("REDIS_URL", "redis://default:O1Z0NHQMsRSJpanSKiQo6Qb7d6KLrUno@redis-16009.c100.us-east-1-4.ec2.redns.redis-cloud.com:16009")
-
 	redisURL := os.Getenv("REDIS_URL")
 	if redisURL == "" {
 		panic("REDIS_URL environment variable not set")
