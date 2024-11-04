63a7cdd585f2cbc6f053c5f23c66a499c4e0f4ff
yinziyang <yinziyangphone@gmail.com>
2024-11-04 09:50:07 +0800

    Add files via upload

--- api/init.go
+++ api/init.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"os"
-	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -10,8 +9,6 @@ import (
 var redisClient *redis.Client
 
 func init() {
-	os.Setenv("REDIS_URL", "redis://default:O1Z0NHQMsRSJpanSKiQo6Qb7d6KLrUno@redis-16009.c100.us-east-1-4.ec2.redns.redis-cloud.com:16009")
-
 	redisURL := os.Getenv("REDIS_URL")
 	if redisURL == "" {
 		panic("REDIS_URL environment variable not set")
@@ -23,14 +20,14 @@ func init() {
 	opt.PoolSize = 10
 	opt.MinIdleConns = 3
 
-	go func() {
-		for {
-			if row, err := callCaibai(); err == nil {
-				insertToRedis(row)
-			}
-			time.Sleep(10 * time.Minute)
-		}
-	}()
+	// go func() {
+	// 	for {
+	// 		if row, err := callCaibai(); err == nil {
+	// 			insertToRedis(row)
+	// 		}
+	// 		time.Sleep(10 * time.Minute)
+	// 	}
+	// }()
 
 	redisClient = redis.NewClient(opt)
 }
